798860d0f947e85cca5b36900c82935c27c04bc4
febdy <BIT-USER@DESKTOP-81R6F60>
2018-01-10 10:25:44 +0900

    1st, 2nd. solved.

--- src/com/javaex/problem01/CalcApp.java
+++ src/com/javaex/problem01/CalcApp.java
@@ -1,8 +1,59 @@
 package com.javaex.problem01;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-    }
+		int a = 0, b = 0;
+		String input = "", op = "";
+		String[] inputSplit;
+		boolean running = true;
+		Add add = new Add();
+		Sub sub = new Sub();
+		Mul mul = new Mul();
+		Div div = new Div();
+
+		while (running) {
+			System.out.print(">> ");
+
+			input = sc.nextLine();
+
+			if (input.equals("/q")) {
+				System.out.println("종료합니다.");
+				running = false;
+			} else {
+				inputSplit = input.split(" ");
+
+				a = Integer.valueOf(inputSplit[0]);
+				op = inputSplit[1];
+				b = Integer.valueOf(inputSplit[2]);
+
+				switch (op) {
+				case "+":
+					add.setValue(a, b);
+					System.out.println(">> " + add.calculate());
+					break;
+				case "-":
+					sub.setValue(a, b);
+					System.out.println(">> " + sub.calculate());
+					break;
+				case "*":
+					mul.setValue(a, b);
+					System.out.println(">> " + mul.calculate());
+					break;
+				case "/":
+					div.setValue(a, b);
+					System.out.println(">> " + div.calculate());
+					break;
+				default:
+					System.out.println("알 수 없는 연산입니다.");
+					break;
+				}
+			}
+
+		}
+	}
 }
--- src/com/javaex/problem02/FriendApp.java
+++ src/com/javaex/problem02/FriendApp.java
@@ -1,10 +1,34 @@
 package com.javaex.problem02;
 
+import java.util.*;
+
 public class FriendApp {
 
-    public static void main(String[] args) {
+	private static final int n = 3;
 
-       
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Friend> friends = new ArrayList<>();
+		String name, hp, school;
+
+		System.out.println("친구를 " + n + "명 등록해주세요.");
+
+		for (int i = 0; i < n; i++) {
+			name = sc.next();
+			hp = sc.next();
+			school = sc.next();
+
+			Friend friend = new Friend();
+			friend.setName(name);
+			friend.setHp(hp);
+			friend.setSchool(school);
+
+			friends.add(friend);
+		}
+
+		for (int i = 0; i < n; i++) {
+			friends.get(i).showInfo();
+		}
+	}
 
 }
